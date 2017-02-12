1293385db408eac92f6a14759f85c7a83e6eff8b
pramodvadiraj <pramod.vadiraj@sprinklr.com>
2017-02-13 01:30:43 +0530

    ThreadUsage

---
+++ src/main/java/Threads.java
@@ -0,0 +1,28 @@
+/**
+ * Created by pramodvadiraj on 13/02/17.
+ */
+
+//depicting usages of Threads, synchronized
+class RunnableObject implements Runnable {
+
+    public  void run() {
+        disp();
+    }
+    public synchronized void disp()
+    {
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            System.out.println("Thread Name " +Thread.currentThread().getName() + " : "+ i);
+
+        }
+    }
+}
+public class Threads
+{
+    public static void main(String[] args) {
+        new Thread(new RunnableObject(),"FirstThread").start();
+        new Thread(new RunnableObject(),"SecondThread").start();
+
+    }
+}
+
